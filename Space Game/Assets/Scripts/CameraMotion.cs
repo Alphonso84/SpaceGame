770d2ed770b3c51f6f817aa25f49045dcbc5bfc3
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-12-29 08:03:59 -0800

    First Commit

---
+++ Space Game/Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    public GameObject objectToFollow;
+    public GameObject[] backgrounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        print(objectToFollow.transform.position.y);
+     if (objectToFollow.transform.position.y <= 200 && objectToFollow.transform.position.y >= 10)
+        {
+            //Make Camera follow player
+            transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y + Time.deltaTime , -90);
+        } else
+        {
+            transform.position = new Vector3(-2.11f, 0f, -90f);
+        }
+    }
+}
---
+++ Space Game/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+    //private float backgroundMaxYPos = -58.58f;
+    //private float backgroundXPos;
+    private readonly float speed = 15f;
+    private readonly float horizontalSpeed = 20f;
+    public GameObject[] background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+        if (Input.GetKey("left")) {
+            transform.Translate(Vector2.left * Time.deltaTime * horizontalSpeed);
+           
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * horizontalSpeed);
+        }
+        if (Input.GetKey("up"))
+        {
+            background[0].transform.Translate(Vector2.down * Time.deltaTime * 5f);
+            background[1].transform.Translate(Vector2.down * Time.deltaTime * 5f);
+        }
+        if (transform.position.y >= 238.6)
+        {
+            print("Move Ship");
+            transform.position = new Vector3(-3.7f, 0f, -1);
+        }
+    }
+}
